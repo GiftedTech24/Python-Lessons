30b43b8ae106187fbb59de7dd913e3556c95326b
GiftedTech24 <onyibabe2424@gmail.com>
2026-07-13 13:48:25 -0700

    Completed Lesson 4 calculator challenge

---
+++ Lesson4.py
@@ -0,0 +1,35 @@
+name = "Gift Joy"
+age = 24
+height = 5.1
+is_student = True
+
+print("Name:", name)
+print("Age:", age)
+print("Height:", height)
+print("Student:", is_student)
+
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(is_student))
+
+num1 = 20
+num2 = 5
+
+print("Addition:", num1 + num2)
+print("Subtraction:", num1 - num2)
+print("Multiplication:", num1 * num2)
+print("Divison:", num1 / num2)
+print("Floor Division:", num1 // num2)
+print("Remainder:", num1 % num2)
+print("Power:", num1 ** num2)
+
+a = 10
+b = 15
+
+print(a == b)
+print(a != b)
+print(a < b)
+print(a > b)
+print(a <= b)
+print(a >= b)
--- lesson3.py
+++ lesson3.py
@@ -3,3 +3,11 @@ course = input("what course do you study? ")
 
 print("Welcome,", name)
 print("You study", course)
+
+age = input("How old are you? ")
+nationality = input("What is your nationality? ")
+level = input("What level are you? ")
+
+print("You are", age)
+print("You are from", nationality)
+print("You are in", level)
