66debdb5cf537f55a03de56f02c51a7cc3f9a1d2
GiftedTech24 <onyibabe2424@gmail.com>
2026-07-19 22:18:42 +0100

    Updated Lesson 6

---
+++ Lesson6.py
@@ -0,0 +1,31 @@
+for number in range(1, 6):
+    print(number)
+
+names = ["Gift", "Ada", "Mary", "John"]
+for name in names: 
+        print(name)
+
+for number in range(1, 11): 
+        print(number)
+    
+for number in range(2, 11, 2):
+    print(number)
+
+goals = ["Learn python", "Build robots", "Learn AI", "Create projects", "Become an Engineer"]
+for goals in goals:
+      print(goals)
+
+count = 1
+
+while count <=5:
+      print(count)
+      count = count + 1
+
+number = int(input("Enter a number: "))
+
+multiplication = 1
+
+while multiplication <=10:
+      print(number, "x", multiplication, "=", number * multiplication)
+      multiplication = multiplication + 1
+
